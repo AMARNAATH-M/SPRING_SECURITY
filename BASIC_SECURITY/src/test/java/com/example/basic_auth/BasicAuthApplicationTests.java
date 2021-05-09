676e364043686ff3ff6dd0f3084e60569bd129bd
AMARNAATH <amarnaath200019@gmail.com>
2021-05-09 22:18:43 +0530

    END OF BASIC SECURITY SECTION

--- BASIC_SECURITY/src/test/java/com/example/basic_auth/BasicAuthApplicationTests.java
+++ BASIC_SECURITY/src/test/java/com/example/basic_auth/BasicAuthApplicationTests.java
@@ -2,12 +2,31 @@ package com.example.basic_auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class BasicAuthApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(new BCryptPasswordEncoder().encode("password"));
+        System.out.println(new Pbkdf2PasswordEncoder().encode("password"));
+        System.out.println(new SCryptPasswordEncoder().encode("password"));
+
+        Map<String,PasswordEncoder> encoders = new HashMap<String,PasswordEncoder>();
+        encoders.put("bcrypt",new BCryptPasswordEncoder());
+        encoders.put("scrypt",new SCryptPasswordEncoder());
+        encoders.put("pbkd",new Pbkdf2PasswordEncoder());
+        System.out.println("");
+        System.out.println(new DelegatingPasswordEncoder("bcrypt",encoders).encode("password"));
+
     }
 
 }
